4f46baa1918d3a1c4a59135c8cd75c02555ad67c
Kurt Kilpela <kurt.kilpela@gemtalksystems.com>
2021-06-03 15:22:24 -0700

    Command line testing scripts

---
+++ scripts/runTests.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+### Run FileSystem tests in a Rowan-enabled stone
+
+export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+topaz -l -I ${SCRIPT_DIR}/login.topaz  -S ${SCRIPT_DIR}/runTests.topaz 
+if [ $? = 0 ]
+    then
+        exit 0
+    else
+        echo !!!!!!!!!!!!!!
+        echo TESTS FAILED
+        echo !!!!!!!!!!!!!!
+        exit 1
+    fi
