afd849094ccaec759d09fcf4066e792a780dd76b
Kurt Kilpela <kurt.kilpela@gemtalksystems.com>
2022-01-10 09:21:08 -0800

    Implemented MacOSFileOpeningOptions created LinuxFileOpeningOptions

--- utilities/open.c
+++ utilities/open.c
@@ -27,6 +27,17 @@ int main() {
 //  printf("O_SYMLINK\n\tvalue:%07o\n", O_SYMLINK);
 //  printf("O_EVTONLY\n\tvalue:%07o\n", O_EVTONLY);
   printf("O_CLOEXEC\n\tvalue:%07o\n", O_CLOEXEC);
+  printf("O_ACCMODE\n\tvalue:%07o\n", O_ACCMODE);
+  printf("O_ASYNC\n\tvalue:%07o\n", O_ASYNC);
+  printf("O_DSYNC\n\tvalue:%07o\n", O_DSYNC);
+  printf("O_DIRECTORY\n\tvalue:%07o\n", O_DIRECTORY);
+  //printf("O_NOATIME\n\tvalue:%07o\n", O_NOATIME);
+  printf("O_NOCTTY\n\tvalue:%07o\n", O_NOCTTY);
+  printf("O_NOFOLLOW\n\tvalue:%07o\n", O_NOFOLLOW);
+  printf("O_NONBLOCK\n\tvalue:%07o\n", O_NONBLOCK);
+//  printf("O_PATH\n\tvalue:%07o\n", O_PATH);
+  printf("O_SYNC\n\tvalue:%07o\n", O_SYNC);
+//  printf("O_TMPFILE\n\tvalue:%07o\n", O_TMPFILE);
 
   printf("O_WRONLY|O_CREAT\n\tvalue:%07o\n", O_WRONLY|O_CREAT);
   int fd = open("/tmp/test.dir", O_WRONLY|O_CREAT, 0777);
