541445eb1ba9cbca17bb5abb8bc2c69ac84dd686
Kurt Kilpela <kurt.kilpela@gemtalksystems.com>
2025-07-14 14:30:34 -0700

    140: Cleanup FsUnixPrimitive and library files.

--- lib/stat.c
+++ lib/stat.c
@@ -8,11 +8,11 @@ int gs_fs_fstat(int fd, struct stat *statbuf) {
   return fstat(fd, statbuf);
 }
 
-int gs_fs_stat(const char *pathname, struct stat *statbuf) {
+int gs_fs_lstat(const char *pathname, struct stat *statbuf) {
   return stat(pathname, statbuf);
 }
 
-int gs_fs_lstat(const char *pathname, struct stat *statbuf) {
+int gs_fs_stat(const char *pathname, struct stat *statbuf) {
   return stat(pathname, statbuf);
 }
 
--- lib/unistd.c
+++ lib/unistd.c
@@ -1,7 +1,19 @@
 #include <unistd.h>
 
+int gs_fs_chdir(const char *path) {
+  return chdir(path);
+}
+
+int gs_fs_chown(const char *path, uid_t owner, gid_t group) {
+  return chown(path, owner, group);
+}
+
 int gs_fs_close(int fd) {
   return close(fd);
+}
+
+int gs_fs_ftruncate(int fd, off_t length) {
+  return ftruncate(fd, length);
 }
 
 char *gs_fs_getcwd(char *buf, size_t size) {
@@ -12,6 +24,10 @@ off_t gs_fs_lseek(int fd, off_t offset, int whence) {
   return lseek(fd, offset, whence);
 }
 
+ssize_t gs_fs_pread(int fd, void *buf, size_t count, off_t offset) {
+  return pread(fd, buf, count, offset);
+}
+
 ssize_t gs_fs_read(int fd, void *buf, size_t count) {
   return read(fd, buf, count);
 }
@@ -20,8 +36,8 @@ int gs_fs_rmdir(const char *path) {
   return rmdir(path);
 }
 
-int gs_fs_ftruncate(int fd, off_t length) {
-  return ftruncate(fd, length);
+int gs_fs_symlink(const char *target, const char *linkpath) {
+  return symlink(target, linkpath);
 }
 
 int gs_fs_unlink(const char *path) {
@@ -30,21 +46,5 @@ int gs_fs_unlink(const char *path) {
 
 ssize_t gs_fs_write(int fd, const void *buf, size_t count) {
   return write(fd, buf, count);
-}
-
-int gs_fs_chown(const char *path, uid_t owner, gid_t group) {
-  return chown(path, owner, group);
-}
-
-int gs_fs_chdir(const char *path) {
-  return chdir(path);
-}
-
-ssize_t gs_fs_pread(int fd, void *buf, size_t count, off_t offset) {
-  return pread(fd, buf, count, offset);
-}
-
-int gs_fs_symlink(const char *target, const char *linkpath) {
-  return symlink(target, linkpath);
 }
 
